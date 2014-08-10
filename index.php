039924ae4eaf0cca15932f9da866efbfa4235599
Alfred <kanalfred@gmail.com>
2014-08-10 16:50:47 -0700

    test

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
   <div class="row">
     <div class="large-4 columns">
       <img src="http://placehold.it/400x300&text=[img]"/>
-      <h4>HIHI is a content section.</h4>
+      <h4>HIHIhihi is a content section.</h4>
       <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
     </div>
     
