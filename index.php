516f7550a98b4d4cfd93172ffdea74cadc58c9d2
Alfred <kanalfred@gmail.com>
2014-08-10 00:39:43 -0700

    revert lines

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
 	echo "wire.watervpn.com";	
 	echo "local insert new text";	
-	echo "=================";
-	echo "local insert new text";	
-	echo "=================";
-	echo "local insert new text";	
-	echo "local insert new text";	
-	echo "=================";
 ?>
