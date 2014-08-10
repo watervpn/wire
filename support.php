99b96a8402fc68488bb7fd3de34abfc8c8d18226
Yan Yong <yan@cheekymonkeymedia.com>
2014-08-10 12:04:20 -0700

    init wireframe

---
+++ support.php
@@ -0,0 +1,128 @@
+<?php include("inc/header.inc"); ?> 
+ 
+  <div class="row">
+
+    <div class="row">
+      <div class="small-12 show-for-small"><br>
+        <img src="http://placehold.it/1000x600&text=For Small Screens"/>
+      </div>
+    </div>
+ 
+ 
+   
+ 
+    </div>
+  </div><br>
+ 
+  <div class="row">
+    <div class="large-12 columns">
+      <div class="row">
+ 
+     
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail"/>
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail"/>
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail"/>
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+        <div class="large-3 small-6 columns">
+          <img src="http://placehold.it/250x250&text=Thumbnail"/>
+          <h6 class="panel">Description</h6>
+        </div>
+ 
+     
+ 
+      </div>
+    </div>
+  </div>
+ 
+ 
+ 
+  <div class="row">
+    <div class="large-12 columns">
+      <div class="row">
+ 
+     
+ 
+        <div class="large-8 columns">
+          <div class="panel radius">
+ 
+          <div class="row">
+          <div class="large-6 small-6 columns">
+ 
+            <h4>Header</h4><hr/>
+            <h5 class="subheader">Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum odio ornare sagittis.
+            </h5>
+ 
+          <div class="show-for-small" align="center">
+            <a href="#" class="small radius button">Call To Action!</a><br>
+ 
+            <a href="#" class="small radius button">Call To Action!</a>
+          </div>
+ 
+          </div>
+          <div class="large-6 small-6 columns">
+ 
+            <p>Suspendisse ultrices ornare tempor. Aenean eget ultricies libero. Phasellus non ipsum eros. Vivamus at dignissim massa. Aenean dolor libero, blandit quis interdum et, malesuada nec ligula. Nullam erat erat, eleifend sed pulvinar ac. Suspendisse ultrices ornare tempor. Aenean eget ultricies libero.
+          </p>
+        </div>
+ 
+        </div>
+        </div>
+        </div>
+ 
+          <div class="large-4 columns hide-for-small">
+ 
+          <h4>Get In Touch!</h4><hr/>
+ 
+          <a class="large button expand" href="#">
+            Call To Action!
+          </a>
+ 
+          <a class="large button expand" href="#">
+            Call To Action!
+          </a>
+ 
+ 
+        </div>
+ 
+     
+ 
+      </div>
+    </div>
+  </div>
+ 
+     
+ 
+  <footer class="row">
+    <div class="large-12 columns">
+      <hr>
+      <div class="row">
+        <div class="large-6 columns">
+          <p>© Copyright no one at all. Go to town.</p>
+        </div>
+        <div class="large-6 columns">
+          <ul class="inline-list right">
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+            <li><a href="#">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+    
+
+
+<?php include("inc/footer.inc"); ?>
