99a46295d6c5cbc53e79280c68cfda5966b50629
Yan Yong <yan@cheekymonkeymedia.com>
2014-08-16 21:23:36 -0700

    Adding login logout and user page

---
+++ login.php
@@ -0,0 +1,59 @@
+<?php 
+if (isset($_POST['username'])) {
+  session_start();
+  $_SESSION['user_role'] = $_POST['username'];
+  
+  switch ($_SESSION['user_role']) {
+    case 'admin':
+      $_GET['dest'] = 'user';
+      break;
+    case 'customer':
+      $_GET['dest'] = 'user';
+      break;
+  }
+
+  if (isset($_GET['dest'])) {
+    header("Location: " . $_GET['dest'] . ".php");
+  }
+  else {
+    header("Location: index.php");
+  }
+  die();
+}
+?>
+
+
+<?php include("inc/header.inc"); ?> 
+
+
+<div class="row">
+<h2>Please Sign in</h2>
+
+<form method="post" action=""> 
+	<div class="row"> 
+		<div class="large-6 columns"> 
+			<label>User name<input name="username" type="text" placeholder="username" /> </label> 
+			<label>Password<input type="text" placeholder="password" /> </label> 
+		</div> 
+	</div> 
+ 
+
+	<div class="row"> 
+		<div class="large-12 columns"> 
+			<p>Forget Password? <a href="#">Click here</a></p>		
+		</div>
+	</div> 
+
+  <div class="row">
+    <div class="large-12 columns">
+      <input class="button" type="submit" value="Log in">
+    </div>
+  </div>
+
+</form>
+
+</div>
+
+
+
+<?php include("inc/footer.inc"); ?>
---
+++ user.php
@@ -0,0 +1,8 @@
+<?php include("inc/header.inc"); ?> 
+
+<div class="row">
+	<a href="logout.php" class="button right">Logout</a>
+</div>
+
+
+<?php include("inc/footer.inc"); ?>
