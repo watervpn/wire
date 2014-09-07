791b12a5278d9cfa07d6839ed9a0219ec34c5910
Yan Yong <yan@cheekymonkeymedia.com>
2014-09-07 09:56:37 -0700

    virtual update

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 
 
 <!-- Main Page Content -->
+<div class="slider_section">
   <div class="row">
     <div class="large-12 columns">
     <div id="slider">
       <!-- <img src="http://placehold.it/1000x400&text=[ img 1 ]"/> -->
       <?php include("inc/slider.inc"); ?>
     </div>
-    
-    <hr/>
     </div>
   </div>
-  
+</div>
  
+<div class="pod_section">
   <div class="row">
     <div class="status large-4 columns">
-      <img src="http://placehold.it/50x50&text=[img]"/>
+      <img src="http://placehold.it/200x200&text=[img]"/>
       <h4>Total Servers:</h4>
       <ul>
         <li>Canada: 3</li>
@@ -26,32 +26,32 @@
       </ul>
     </div>
     
-    <div class="large-4 columns">
-      <img src="http://placehold.it/50x50&text=[img]"/>
+    <div class="status large-4 columns">
+      <img src="http://placehold.it/200x200&text=[img]"/>
       <h4>Total bendwidth aviable:</h4>
       <p>200,000 MB</p>
     </div>
     
-    <div class="large-4 columns">
-      <img src="http://placehold.it/50x50&text=[img]"/>
+    <div class="status large-4 columns">
+      <img src="http://placehold.it/200x200&text=[img]"/>
       <h4>Fastest connection:</h4>
       <p>on August 31st 2014, Canada. 140MB by anynonmouse user.</p>
     </div>
   
   </div>
-    
+</div>
  
-  <div class="row">
-    <div class="large-12 columns">
-      <div class="panel">
-        <h4>Get in touch!</h4>         
+  <div class="">
+    <div class="cta large-12 columns">
+      <div class="">
         <div class="row">
-          <div class="large-9 columns">
-            <p>We'd love to hear from you, you attractive person you.</p>
-          </div>
-          <div class="large-3 columns">
-            <a href="#" class="radius button right">Contact Us</a>
-          </div>
+          <div class="large-6 cta_heading">
+            <h4 class="">Keep infomed.</h4>
+            <p class="">We provide weekly updates about privercy alerts.</p>
+            </div>
+          <div class="large-3 cta_button">
+            <a href="#" class="radius button">Sign up now</a>
+          </div>   
         </div>
       </div>  
     </div>
